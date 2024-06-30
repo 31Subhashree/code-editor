3977447910360d6139435425bd404cdae5196e07
31Subhashree <48314407+31Subhashree@users.noreply.github.com>
2024-06-30 20:44:38 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CodeEditor from './CodeEditor';
+import './App.css'; 
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Code Editor</h1>
+      <CodeEditor />
+    </div>
+  );
+};
+
+export default App;
